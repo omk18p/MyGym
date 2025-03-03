1c0c586f3c326cb11c64563934d52e64b3c6c118
omk18p <omkar003628p@gmail.com>
2025-03-03 12:01:00 +0530

    feat: improve error message UX

--- .activity.log.js
+++ .activity.log.js
@@ -68,3 +68,4 @@
 // commit 1 on 2025-03-06: chore: rename component
 // commit 2 on 2025-03-06: docs: update README
 // commit 1 on 2025-03-05: ci: fix deploy script
+// commit 1 on 2025-03-03: feat: improve error message UX
