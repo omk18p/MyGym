939b84d928dc0faf8590c7dd2b809885dba5d0e2
omk18p <omkar003628p@gmail.com>
2025-04-14 12:01:00 +0530

    fix: improve null check

--- .activity.log.js
+++ .activity.log.js
@@ -43,3 +43,4 @@
 // commit 1 on 2025-04-18: style: center align text
 // commit 2 on 2025-04-18: refactor: simplify async call
 // commit 1 on 2025-04-16: docs: add API documentation
+// commit 1 on 2025-04-14: fix: improve null check
