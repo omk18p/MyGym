67ef2952c6ace615f372cf72b22dc1345e4ff4a1
omk18p <omkar003628p@gmail.com>
2025-07-08 12:02:00 +0530

    feat: implement copy-to-clipboard

--- .activity.log.js
+++ .activity.log.js
@@ -1,3 +1,4 @@
 // commit 1 on 2025-07-09: refactor: extract utility method
 // commit 2 on 2025-07-09: update: rename functions for clarity
 // commit 1 on 2025-07-08: update: change favicon
+// commit 2 on 2025-07-08: feat: implement copy-to-clipboard
